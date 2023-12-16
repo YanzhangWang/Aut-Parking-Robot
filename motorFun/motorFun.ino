dac2a8c80f2818302020299797f00240d563edb1
Yanzhang.Wang <2771757163@qq.com>
2023-12-16 23:41:41 +0800

    motorcode

---
+++ motorFun/motorFun.ino
@@ -0,0 +1,62 @@
+const byte ENA=6;
+const byte ENB=5;
+const byte IN1=3;
+const byte IN2=2;
+const byte IN3=1;
+const byte IN4=0;
+
+int MAspeed = 120;
+int MBspeed = 120;
+
+void MAclock(){
+  analogWrite(ENA, MAspeed);
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+}
+
+void MAanticlock(){
+  analogWrite(ENA, MAspeed);
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+}
+
+void MBclock(){
+  analogWrite(ENB, MBspeed);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+}
+
+
+void MBanticlock(){
+  analogWrite(ENB, MBspeed);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+}
+
+void forward(){
+  MBclock();
+  MAanticlock();
+}
+
+void backward(){
+  MAclock();
+  MBanticlock();
+}
+
+
+
+
+void setup() {
+  pinMode(ENA, OUTPUT);
+  pinMode(ENB, OUTPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
