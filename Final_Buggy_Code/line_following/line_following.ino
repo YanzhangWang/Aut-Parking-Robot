0491b3ed781074b21b9a29a584a3925d3e5f2785
gtcodes22 <117656101+gtcodes22@users.noreply.github.com>
2024-02-22 15:36:18 +0000

    Add files via upload

---
+++ Final_Buggy_Code/line_following/line_following.ino
@@ -0,0 +1,32 @@
+// A0:
+
+// the setup routine runs once when you press reset:
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  // read the input on analog pin 0:
+  int sensorValue = analogRead(A0);
+  // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
+  float voltage = sensorValue * (5.0 / 1024.0);
+
+  // the lower the voltage, the brighter it is
+  if ((voltage >= 0) && (voltage <= 0.4)) {
+    Serial.print ("it is dark - ");
+  } else if ((voltage > 0.4) && (voltage <= 2)) {
+    Serial.print ("it is bright - ");
+  } else {
+    Serial.print ("it is bright - ");
+  }
+  // print out the value you read:
+  Serial.println(voltage);
+
+// D0:
+
+// when sensor pin D0 is connected, the sensor only knows the state light (0.14V) and dark (5.0V).
+// The brightness at which the particular state is to be set can be set using the rotary potentiometer.
+
+}
---
+++ Final_Buggy_Code/ultrasonic/ultrasonic.ino
@@ -0,0 +1,27 @@
+const int TrigPin=8;           //Set pin 8 as the launching pin position
+const int EchoPin=9;           //Set pin 9 as the receiving pin position
+float cm;                 
+
+void setup() {
+Serial.begin(9600);
+pinMode(TrigPin,OUTPUT);
+pinMode(EchoPin,INPUT);
+}
+
+void loop() {
+digitalWrite(9, LOW);
+
+digitalWrite(TrigPin,LOW);        //Send level pulses to the Trigpin pins in low and high order
+delayMicroseconds(2);             //Delay time
+digitalWrite(TrigPin,HIGH);
+delayMicroseconds(10);
+digitalWrite(TrigPin,LOW);
+cm=pulseIn(EchoPin,HIGH)/58.0;    //Read the pulse width and convert it to centimeters
+Serial.print(cm);
+if(cm<=40.0){
+  digitalWrite(9, HIGH);
+}
+Serial.println(); 
+
+delay(1000);
+}
